e4406d8696faf7068edbba47044a9ba872089f67
Bhushan Sonawane <bhushan.prod@gmail.com>
2020-09-28 13:47:08 +0530

    Add files via upload

---
+++ Models/Promotion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionApp.Model
+{
+    public class Promotion
+    {
+        public int PromotionId { get; set; }
+        public PromotionType Type { get; set; }
+
+        /// <summary>
+        /// Collection of Sku and Quantity participating in promotion.
+        /// </summary>
+        public Dictionary<string, int> Skus { get; set; }
+        
+        public int OfferPrice { get; set; }
+    }
+
+    public enum PromotionType
+    {
+        ComboOffer,
+        QuantityDiscount,
+        SeasonOffer,
+        None
+    }
+}
